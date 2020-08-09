3aa8f276395cfc2aba38e4e3db5de3a058b2e466
Michal Dziadkowiec <michal.dziadkowiec@motorolasolutions.com>
2020-08-09 12:56:47 +0200

    Add POC of new user flow
    - add logic for adding new user
    - add logic for disconecting user
    - keep track of all active users
    - handle scenario when user already joined chat

--- client/js/index.js
+++ client/js/index.js
@@ -1,8 +1,27 @@
-import io from 'socket.io-client';
 import { showAlert } from './views/test-view';
+import EVENTS from '../../common/socket-events';
+import socket from './socket';
 
-const socket = io('http://localhost:3000');
+const handleAddUser = (e) => {
+  e.preventDefault();
 
-console.log(socket);
+  const userName = document.querySelector('form > input').value;
+  console.log(userName);
+  socket.emit(EVENTS.USER_ADD, userName);
+};
+
+const displayUserData = ({ userName, numberOfUsers, activeUsers }) => {
+  console.log(userName, numberOfUsers, activeUsers);
+  document.getElementById('user-name').textContent = userName;
+  document.getElementById('number-of-users').textContent = numberOfUsers;
+};
+
+socket.on(EVENTS.USER_JOINED, displayUserData);
+
+socket.on(EVENTS.USER_EXISTS, ({ userName }) => {
+  alert(`${userName} is already active in the chat!`);
+});
+
+document.querySelector('form').addEventListener('submit', handleAddUser);
 
 document.querySelector('.btn').addEventListener('click', showAlert);
---
+++ common/socket-events.js
@@ -0,0 +1,12 @@
+/**
+ * @fileoverview This file contains a single place of truth for the Socket.io event names
+ */
+
+const EVENTS = {
+  USER_ADD: 'USER_ADD',
+  USER_EXISTS: 'USER_EXISTS',
+  USER_JOINED: 'USER_JOINED',
+  CHAT_MESSAGE_SENT: 'CHAT_MESSAGE_SENT',
+};
+
+export default EVENTS;
